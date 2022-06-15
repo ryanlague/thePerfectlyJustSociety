6a87b9e48372e686c22fb83c8d6be729019a5b2a
Ryan Lague <ryanlaguemusic@gmail.com>
2022-06-14 20:30:19 -0400

    Fixed description

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='thePerfectlyJustSociety',
-    version='1.0.1',
+    version='1.0.2',
     license='MIT',
     author='Ryan Lague',
     author_email='ryanlague@hotmail.com',
